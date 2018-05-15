f42fcc6685a835e2ff8d12c1b83b9ce31845e7a5
smpldvd <david.n.luu@gmail.com>
2018-05-14 23:59:12 -0500

    Clean up codes.

--- assets/javascipt/game.js
+++ assets/javascipt/game.js
@@ -1,41 +1,47 @@
 // List the array to choose from
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 // Set all to starting position
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var lettersPicked = [];
-var winningLetter = [];
+var winningLetter = computerChoice;
 
+// Pick a letter from the alphabet
+var compLetter = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+winningLetter.push(compLetter);
+console.log(winningLetter);
 
 // Letters pick subtracts guesses
 document.onkeyup = function (event) {
+    var userKeyed = (event.key).toLowerCase();
 
-    // Pick a letter from the alphabet
-    let computerChoice = letters[Math.floor(Math.random() * letters.length)];
-    winningLetter.push(computerChoice);
-    console.log(winningLetter);
-
-    var inputKey = (event.key).toLowerCase();
-    document.getElementById("letters").innerHTML = inputKey + ", ";
-    console.log(inputKey)
-
-    if ((inputKey === computerChoice) && (guessesLeft > 0)) {
-        document.getElementById("wins").innerHTML = wins++;
+    if ((userKeyed === compLetter) && (guessesLeft > 0)) {
+        alert("You Win!");
+        wins++;
         guessesLeft = 9;
-        lettersPicked = [];
+        var newCompLetter = letterChoices[Math.floor(Math.random) * letterChoices.length];
+        compLetter = newCompLetter;
     }
-
-    if (letters.indexOf(inputKey) === -1) return;
-    
-    else if ((inputKey !== computerChoice) && (guessesLeft > 0)) {
-        document.getElementById("guessesLeft").innerHTML = guessesLeft--;
-    }
-    
     else {
-        document.getElementById("losses").innerHTML = losses++;
-        guessesLeft = 9;
-        lettersPicked = [];
+        guessesLeft--;
+        document.getElementById("letters").innerHTML = "Your Guesses so far: " + userKeyed;
+        if (guessesLeft > 0) {
+            lettersPicked.push(userKeyed);
+            document.getElementById("letters").innerHTML = "Your Guesses so far: " + list.toString() + " ";
+        }
     }
+    if (guessesLeft === 0) {
+        alert("You lose! The correct letter was " + compLetter);
+        losses++;
+        guessesLeft = 9;
+        var newCompLetter = letterChoices[Math.floor(Math.random) * letterChoices.length];
+        compLetter = newCompLetter;
+    }
+
+    document.getElementById("wins").innerHTML = "Wins : " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("guessesLeft").innerHTML = "Guesses left : " + guessesLeft;
+
 }
